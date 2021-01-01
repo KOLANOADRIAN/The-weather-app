b651f8fd83ee75733ad426894e348691c1117d38
Adrian Kolano <kolanoadrian@hotmail.com>
2021-01-01 20:31:37 +0100

    add @media in style and separate containers

--- script.js
+++ script.js
@@ -16,19 +16,24 @@ const getAPI = (event) => {
         .then((json) => {
             let rezultat = json;
             console.log(rezultat),
+           
             container__result.innerHTML = (`
-    <span class = "stacja"> Stacja: ${rezultat.stacja}<span><br>
-    <span class = "data"> Data Pomiaru:  ${rezultat.data_pomiaru}<span><br>
-    <span class = "godzina"> Godzina pomiaru: ${rezultat.godzina_pomiaru}<span><br>
-    <span class = "cisnienie"> Ciśnienie: ${rezultat.cisnienie} hp<span><br>
-    <span class = "temperatura"> Temperatura: ${rezultat.temperatura} celciusza<span><br>
-    <span class = "opady"> Opady: ${rezultat.suma_opadu} w cm na 1 m2<span><br>
-    <span class = "wilgotność"> Wilgotność: ${rezultat.wilgotnosc_wzgledna}<span><br>
-    <span class = "kierunek_wiatru"> Kierunek wiatru: ${rezultat.kierunek_wiatru}<span>
+    <span class="stacja"> Stacja: ${rezultat.stacja}<span><br>
+    <span class="data"> Data Pomiaru:  ${rezultat.data_pomiaru}<span><br>
+    <span class="godzina"> Godzina pomiaru: ${rezultat.godzina_pomiaru}:00<span><br>
+    <span class="cisnienie"> Ciśnienie: ${rezultat.cisnienie} hp<span><br>
+    <span class="temperatura"> Temperatura: ${rezultat.temperatura} celciusza<span><br>
+    <span class="opady"> Opady: ${rezultat.suma_opadu} w cm na 1 m2<span><br>
+    <span class="wilgotność"> Wilgotność: ${rezultat.wilgotnosc_wzgledna}<span><br>
+    <span class="kierunek_wiatru"> Kierunek wiatru: ${rezultat.kierunek_wiatru}<span>
     `)
+    if (rezultat.temperatura < 1) {
+        console.log("jest mniej niż 1")
+     }
     })
     .catch((error) => console.log(error, "błąd"));    
     }
     const button = document.querySelector(".button")
     button.addEventListener("click", getAPI)
+    
     
