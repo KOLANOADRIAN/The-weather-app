4a8e64ce3eb09bd0cd53f342e3c5a7478c35427e
ADRIAN <38044318+KOLANOADRIAN@users.noreply.github.com>
2023-01-06 10:12:48 +0100

    refactoring

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
-const sayHello = () => {
-    console.log("Hello FrondEnd__Dewelopers")
-}
-sayHello();
-
-const getAPI = (event) => {
-    event.preventDefault();
-    const container__result = document.querySelector(".container__result")
-    const meteoNumber = document.querySelector('[name = "select__city"]').value;
-    const url = `https://danepubliczne.imgw.pl/api/data/synop/id/${meteoNumber}`;
-    fetch(url)
-        .then((answer) => {
-            console.log(answer)
-            if (answer.status !== 200) {
-                throw Error("to nie jest odpowiedz 200")
-            } else {
-                return answer.json()
-            }
+{
+    const sayHello = () => {
+        window.addEventListener("load", function () {
+            console.log("HelloWorld!... The page has loaded")
         })
-        .then((json) => {
-            let rezultat = json;
-            console.log(rezultat),
+    };
+    sayHello()
     
-            container__result.innerHTML = (`
-    <span class="stacja"> Stacja: ${rezultat.stacja}<span><br>
-    <span class="data"> Data Pomiaru:  ${rezultat.data_pomiaru}<span><br>
-    <span class="godzina"> Godzina pomiaru: ${rezultat.godzina_pomiaru}:00<span><br>
-    <span class="cisnienie"> Ciśnienie: ${rezultat.cisnienie} hp<span><br>
-    <span class="temperatura"> Temperatura: ${rezultat.temperatura} celciusza<span><br>
-    <span class="opady"> Opady: ${rezultat.suma_opadu} w cm na 1 m2<span><br>
-    <span class="wilgotność"> Wilgotność: ${rezultat.wilgotnosc_wzgledna}<span><br>
-    <span class="prędkość"> Prędkość wiatru: ${rezultat.predkosc_wiatru}<span>
-    `)
-    const temp = rezultat.temperatura;
-    if (temp < 0) {
-        console.log("Temperatura poniżej 0")
-     }
-     else {
-        console.log("Temperatura powyżej 0")
-     }
-    })
-    .catch((error) => console.log(error, "błąd"));    
+    const getAPI = (event) => {
+        event.preventDefault();
+        const container__result = document.querySelector(".container__result")
+        const meteoNumber = document.querySelector('[name = "select__city"]').value;
+        const url = `https://danepubliczne.imgw.pl/api/data/synop/id/${meteoNumber}`;
+        fetch(url)
+            .then((answer) => {
+                console.log(answer)
+                if (answer.status !== 200) {
+                    throw Error("to nie jest odpowiedz 200")
+                } else {
+                    return answer.json()
+                }
+            })
+            .then((json) => {
+                let rezultat = json;
+                console.log(rezultat),
+    
+                    container__result.innerHTML = (`
+        <span class="stacja"> Stacja: ${rezultat.stacja}<span><br>
+        <span class="data"> Data Pomiaru:  ${rezultat.data_pomiaru}<span><br>
+        <span class="godzina"> Godzina pomiaru: ${rezultat.godzina_pomiaru}:00<span><br>
+        <span class="cisnienie"> Ciśnienie: ${rezultat.cisnienie} hp<span><br>
+        <span class="temperatura"> Temperatura: ${rezultat.temperatura} celciusza<span><br>
+        <span class="opady"> Opady: ${rezultat.suma_opadu} w cm na 1 m2<span><br>
+        <span class="wilgotność"> Wilgotność: ${rezultat.wilgotnosc_wzgledna}<span><br>
+        <span class="prędkość"> Prędkość wiatru: ${rezultat.predkosc_wiatru}<span>
+        `)
+                const temp = rezultat.temperatura;
+                if (temp < 0) {
+                    console.log("Temperatura poniżej 0")
+                }
+                else {
+                    console.log("Temperatura powyżej 0")
+                }
+            })
+            .catch((error) => console.log(error, "błąd"));
     }
     const button = document.querySelector(".button")
     button.addEventListener("click", getAPI)
-    
-    
+
+}
+
